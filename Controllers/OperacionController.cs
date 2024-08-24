b2227b7b439a53d3115c8c5d53494f039fa78d60
Adriancas28 <castroadrian1228@gmail.com>
2024-08-24 15:27:53 -0500

    cambios2

---
+++ Controllers/OperacionController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PC01_PROGRA.Controllers
+{
+    [Route("[controller]")]
+    public class OperacionController : Controller
+    {
+        private readonly ILogger<OperacionController> _logger;
+
+        public OperacionController(ILogger<OperacionController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
